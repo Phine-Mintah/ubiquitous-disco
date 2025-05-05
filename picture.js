6459e77cdb6c107d8f2b6deb49e54df02dcf6311
unknown <clt7680seal@gmail.com>
2025-05-05 17:23:18 +0100

    working with a js file

---
+++ picture.js
@@ -0,0 +1,9 @@
+function button() {
+    let button = document.getElementById("pixel");
+    button.style.backgroundColor = "black";
+    button.style.color = "white";
+    button.style.height = "100px";
+    // button.style.width = "200px";
+    // button.document.createElement("p")
+    // document.getElementById("pixel").innerHTML = "Hello World";
+}
